e3192adc8107ebcdb0bfa8910f463cfcb7eaaae1
pottergryff8@gmail.com <GABRIELDASILVASANTAN@GSS>
2021-03-02 23:02:50 -0300

    Aplicação Console
    Desenvolver um aplicativo em C# que determine se o cliente de uma loja de departamentos ultrapassou o limite de crédito em uma conta. Para cada cliente, os seguintes fatos estão disponíveis:
    a) o numero da conta
    b) o saldo no inicio do mês
    c) o total de todos os itens cobrados desse cliente no mês corrente
    d) O total de todos os créditos aplicados na conta desse cliente no mês corrente
    e) o limite de credito permitido

---
+++ teste/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] contas = new string[4]{"12345678910","448787646452",
+            "59274521721","268729521"};
+
+            Random numeroAleatorio = new Random();
+            int numeroDaConta = numeroAleatorio.Next(contas.Length);
+            string index = contas[numeroDaConta];
+            Console.WriteLine(numeroDaConta);
+            Console.Write(index);
+        }
+    }
+}
